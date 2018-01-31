75fa30a89fd87428de917509dfd14db37d5bbfb9
Viraj <viraj@Vishnus-iMac.local>
2018-01-31 19:41:00 +0530

    first commit

---
+++ src/com/viraj/Variables/Variable.java
@@ -0,0 +1,29 @@
+package com.viraj.Variables;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// I am outputting the word hello to the console
+	
+		System.out.println("Hello World!!!");
+		
+		// I can enter what ever text that i want
+		// Single line comments
+		
+		/*
+		 *   This is a Multiline comment 
+		 */
+		
+		/**
+		 * This is java Doc Comment
+		 * 
+		 * 
+		 * 
+		 **/
+
+	}
+
+}
